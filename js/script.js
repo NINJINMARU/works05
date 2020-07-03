11ecf1b6f4da1a46976eba9768d09be8091d5ac7
NINJINMARU <64196568+NINJINMARU@users.noreply.github.com>
2020-07-03 23:47:22 +0900

    works05
    works05

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+$(function () {
+  $(".acMenu dt").on("click", function () {
+    $(this).next().slideToggle();
+  });
+
+  $("#top_btn").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 1000, "swing");
+  });
+
+  $('.hl-toggler').click(function () {
+    $(this).hide();
+    $('.hl-item, .hl-closer').fadeIn();
+  });
+  $('.hl-closer').click(function(){
+    $('.hl-closer').fadeOut(3);
+    $('.hl-item').fadeOut(1);
+    $('.hl-toggler').fadeIn();
+  });
+});
